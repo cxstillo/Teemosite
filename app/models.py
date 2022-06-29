6852786eff10419f47534685e2f8a05529782e8d
Diego Arteaga <cvstillo@Diegos-MacBook-Pro.local>
2022-06-29 17:50:23 -0400

    ver2

--- app/models.py
+++ app/models.py
@@ -1,3 +1,10 @@
+from fileinput import filename
 from django.db import models
 
 # Create your models here.
+class gallery(models.Model):
+    image= models.FileField(upload_to='images/',null=True)
+    filename= models.CharField(max_length=225,null=True, blank=True)
+
+    def __str__(self) -> str:
+        return str(self.filename)
--- app/views.py
+++ app/views.py
@@ -1,6 +1,15 @@
 
 from django.shortcuts import render
+from .models import*
+from django.conf import settings
 
 # Create your views here.
 def index(request):
-    return render(request,"app/index.html")
+    ratgallery=gallery.objects.all()
+    context={
+        "ratgallery": ratgallery,
+        "media_url": settings.MEDIA_URL
+
+
+    }
+    return render(request,"app/index.html",context)
